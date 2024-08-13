85d5bbfca77b29c7b5a84089d5d1d7bd6f4572bb
ShishirRao <shishir.t.rao@gmail.com>
2024-08-13 14:32:24 -0400

    Works for Nethravathi. Testing for Kaveri. Adding code to automatically drop irrigation SHPs

--- Connectivity.R
+++ Connectivity.R
@@ -19,11 +19,15 @@ library("shp2graph")
 library(ggrepel)
 
 
-#shape_river <- st_read("Nethravathi/Nethravathi_river.shp")
-shape_river <- st_read("Nethravathi/Nethravathi_river_v2.shp")
+shape_river <- st_read("Nethravathi/Nethravathi_river.shp")
 shape_basin <- st_read("Nethravathi/Nethravathi_wshed.shp")
-#shape_dams <- st_read("Nethravathi/Nethravathi_SHPs.shp")
-shape_dams <- st_read("Nethravathi/Nethravathi_SHPs_v2.shp")
+shape_dams <- st_read("Nethravathi/Nethravathi_SHPs.shp")
+
+#shape_river <- st_read("Kaveri/Kaveri_river.shp")
+#shape_basin <- st_read("Kaveri/Kaveri_sub_basin_Karnataka_wshed.shp")
+#shape_dams <- st_read("Kaveri/Kaveri_SHPs.shp")
+
+
 #### shape files processing ####
 
 ggplot() +
@@ -49,7 +53,7 @@ ggplot() +
   coord_fixed() +
   theme_minimal() +
   ggspatial::layer_spatial(shape_basin, fill = NA, color = "gray90") +
-  #ggspatial::layer_spatial(shape_river_small, aes(color = log10(UPLAND_SKM)) )+
+  ggspatial::layer_spatial(shape_river_small, aes(color = log10(UPLAND_SKM)) )+
   ggspatial::layer_spatial(shape_dams,color = "red") +
   scale_color_viridis(direction = -1, name= "upstream area \n(log10[Km^2])") +
   theme(legend.position = "bottom") +
@@ -114,8 +118,6 @@ dams_snapped <- snap_to_river(shape_dams,
 # Retain dams that were snapped. 
 dams_snapped_reduced <-
   dams_snapped[st_contains(shape_river_simple %>% st_sf(), dams_snapped, prepared = FALSE)[[1]],]
-# something wrong with the above line of code. Not sure why some dams are being dropped even though they all are on the river network.
-
 
 # Check if dams were snapped properly to the network (all the distances should be zero)
 st_distance(dams_snapped_reduced, shape_river_simple) %>% sum
@@ -131,7 +133,7 @@ dams_snapped_joined <- dams_snapped_reduced %>%
   group_by(cluster) %>%
   slice(1) %>%
   ungroup() %>%
-  mutate(id_dam = as.character(1:nrow(.)), pass_u = 0.1, pass_d = 0.2) %>%
+  mutate(id_dam = as.character(1:nrow(.)), pass_u = 0,pass_d = 0) %>%
   as.data.frame %>%
   st_as_sf() %>%
   st_join(., shape_river, join = st_is_within_distance, dist = 10 ) %>% 
@@ -150,6 +152,9 @@ ggplot() +
   theme(legend.position = "bottom") +
   ggspatial::annotation_scale(location = "bl", style = "ticks") +
   ggspatial::annotation_north_arrow(location = "br")
+
+#st_write(dams_snapped, "Kaveri/dams_snapped.shp")
+#st_write(dams_snapped_reduced, "Kaveri/dams_snapped_reduced.shp")
 
 headwaters_checking <- headwaters_dam(dams_snapped_joined, shape_river_simple)
 head(headwaters_checking$flag_headwater)
@@ -285,42 +290,6 @@ river_graph
 igraph::edge_attr(river_graph) %>% names
 ## [1] "type"       "id_links"   "id_barrier" "pass_u"     "pass_d"
 
-igraph::edge_attr(river_graph)$id_links[24]
-igraph::edge_attr(river_graph)$id_barrier[24]
-
-which(igraph::edge_attr(river_graph)$id_barrier == "1")
-which(igraph::edge_attr(river_graph)$id_links == 92)
-
-network_links_df <- st_is_within_distance(network_links, river_net_simplified,
-                                          dist = 0.01) %>%
-  lapply(.,
-         FUN = function(x){data.frame("from" = x[1], "to" = x[2], "to2" = x[3]) }) %>%
-  do.call(rbind,.) %>%
-  cbind(network_links %>% st_drop_geometry())
-
-?graph_from_data_frame
-
-actors <- data.frame(
-  name = c(
-    "Alice", "Bob", "Cecil", "David",
-    "Esmeralda"
-  ),
-  age = c(48, 33, 45, 34, 21),
-  gender = c("F", "M", "F", "M", "F")
-)
-relations <- data.frame(
-  from = c(
-    "Bob", "Cecil", "Cecil", "David",
-    "David", "Esmeralda"
-  ),
-  to = c("Alice", "Bob", "Alice", "Alice", "Bob", "Alice"),
-  same.dept = c(FALSE, FALSE, TRUE, FALSE, FALSE, TRUE),
-  friendship = c(4, 5, 5, 2, 1, 1), advice = c(4, 5, 5, 4, 2, 3)
-)
-g <- graph_from_data_frame(relations, directed = TRUE, vertices = actors)
-print(g, e = TRUE, v = TRUE)
-plot(g)
-
 # check river_graph vertices
 igraph::vertex_attr(river_graph) %>% names
 ## [1] "name"       "length"     "alt"        "DIST_DN_KM" "UPLAND_SKM"
@@ -333,84 +302,6 @@ hist(V(river_graph)$length)
 # update length attribute
 V(river_graph)$name <- as.character(V(river_graph)$name)
 
-
-# Function for organism that prefers high elevation
-suit_fun_high <- function(x){dnorm(x, mean = 1500, sd = 500)*410*sqrt(3*pi)}
-
-# Function for organism that prefers low elevation
-suit_fun_low <- function(x){exp(- 0.001*x)}
-
-# Calculate HSI for the igraph nodes
-V(river_graph)$HSI_low <- suit_fun_low(V(river_graph)$alt)
-V(river_graph)$HSI_high <- suit_fun_high(V(river_graph)$alt)
-
-# Calculate weighted usable length for igraph nodes
-V(river_graph)$WUL_low <- V(river_graph)$HSI_low * V(river_graph)$length
-V(river_graph)$WUL_high <- V(river_graph)$HSI_high * V(river_graph)$length
-
-# Plot the two response functions
-1:10:2500 %>%
-  data.frame("Elevation" = .,
-             "Low" = suit_fun_low(.), 
-             "High" = suit_fun_high(.)) %>%
-  pivot_longer(c("Low", "High"), 
-               names_to = "Type", values_to = "HSI") %>%
-  ggplot() + 
-  geom_line(aes(x = Elevation, y = HSI, color = Type))+
-  theme_bw() + xlab("Elevation (m)") + ylab("Habitat Suitability Index (HSI)")
-
-
-# Extract reaches centroids
-river_net_simplified_centroids <- river_net_simplified %>%
-  st_as_sf() %>%
-  st_centroid() 
-
-# get the centroids coordinates
-coordinates <- river_net_simplified_centroids %>%
-  dplyr::mutate(lat = sf::st_coordinates(.)[,1], lon = sf::st_coordinates(.)[,2]) %>%
-  dplyr::select(lat, lon) %>%
-  st_set_geometry( NULL)
-
-# fortify the igraph object
-gg0 <- ggnetwork(river_graph, layout = coordinates %>% as.matrix(), scale = FALSE)
-
-grid.arrange(
-  ggplot(gg0, aes(x = x, y = y, xend = xend, yend = yend)) +
-    coord_fixed() +
-    geom_nodes(aes(color = HSI_high)) +
-    geom_edges(alpha = 0.5) +
-    scale_color_viridis()+
-    theme_blank()+
-    ggtitle("High-altitude organism") +
-    labs(caption = "Network directionality not shown"), 
-  
-  ggplot(gg0, aes(x = x, y = y, xend = xend, yend = yend)) +
-    coord_fixed() +
-    geom_nodes(aes(color = WUL_high)) +
-    geom_edges(alpha = 0.5) +
-    scale_color_viridis()+
-    theme_blank()+
-    ggtitle("High-altitude organism") +
-    labs(caption = "Network directionality not shown"), 
-  
-  ggplot(gg0, aes(x = x, y = y, xend = xend, yend = yend)) +
-    coord_fixed() +
-    geom_nodes(aes(color = HSI_low)) +
-    geom_edges(alpha = 0.5) +
-    scale_color_viridis()+
-    theme_blank()+
-    ggtitle("Low-altitude organism") +
-    labs(caption = "Network directionality not shown"),
-  
-  ggplot(gg0, aes(x = x, y = y, xend = xend, yend = yend)) +
-    coord_fixed() +
-    geom_nodes(aes(color = WUL_low)) +
-    geom_edges(alpha = 0.5) +
-    scale_color_viridis()+
-    theme_blank()+
-    ggtitle("Low-altitude organism") +
-    labs(caption = "Network directionality not shown"),
-  ncol=2, nrow=2)
 
 # Initialize list where to store all the index calculation outputs
 index <- list()
@@ -426,143 +317,6 @@ index[[1]] <- index_calculation(graph = river_graph,
                                 B_ij_flag = FALSE,
                                 index_type = "sum",
                                 index_mode = "from")
-
 ?index_calculation
 
-sum(shape_river_simplified$length)/ 10000
-
-
-
-
-
-#### dissecting index calculation ####
-function (graph, weight = "length", nodes_id = "name", index_type = "full", 
-          index_mode = "to", c_ij_flag = TRUE, B_ij_flag = TRUE, dir_fragmentation_type = "symmetric", 
-          pass_confluence = 1, pass_u = "pass_u", pass_d = "pass_d", 
-          field_B = "length", dir_distance_type = "symmetric", disp_type = "exponential", 
-          param_u, param_d, param, param_l) 
-
-  if (!igraph::is_igraph(graph)) 
-    stop("'graph' must be an 'igraph' object")
-  if (!(index_type %in% c("full", "reach", "sum"))) 
-    stop("'index_type' must me either 'full', 'reach', or 'sum'")
-  if (index_type == "reach" & !(index_mode %in% c("from", "to"))) 
-    stop("'index_mode' must me either 'from' or 'to'")
-  if (index_type == "reach" & missing(index_mode)) 
-    stop("'index_mode' must me defined when index_type = 'reach'")
-  if (!(weight %in% igraph::vertex_attr_names(graph))) 
-    stop("'weight' argument must be a valid vertex attribute in 'graph'")
-  if (!(nodes_id %in% igraph::vertex_attr_names(graph))) 
-    stop("'nodes_id' argument must be a valid vertex attribute in 'graph'")
-  if (!(c_ij_flag | B_ij_flag)) 
-    stop("at least one among c_if and B_ij should be selected for calculations")
-  if (length(igraph::vertex_attr(graph, nodes_id)) < igraph::gorder(graph)) 
-    stop("'nodes_id' must be unique for each vertex")
-  if (!igraph::is_connected(graph)) 
-    stop("'graph' must be connected (check if some nodes are disconnected with igraph::components() )")
-  if ((dir_fragmentation_type == "asymmetric" | dir_distance_type == 
-       "asymmetric") & igraph::is_directed(graph) == FALSE) 
-    stop("'graph' must be directed when 'dir_fragmentation_type' or 'dir_distance_type' are set to 'asymmetric'")
-  if (weight %in% igraph::edge_attr_names(graph)) 
-    stop("'weight' argument must be a edge attribute in 'graph'")
-  if (field_B %in% igraph::edge_attr_names(graph)) 
-    stop("'field_B' argument must be a edge attribute in 'graph'")
-  if (!is.character(igraph::get.vertex.attribute(graph, nodes_id))) 
-    stop("'nodes_id' attribute of 'graph' must be of type 'charachter'")
-  if (c_ij_flag == TRUE) {
-    if (!(pass_u %in% igraph::edge_attr_names(graph))) 
-      stop("'pass_u' argument must be a edge attribute in 'graph'")
-    if (!(pass_d %in% igraph::edge_attr_names(graph))) 
-      stop("'pass_d' argument must be a edge attribute in 'graph'")
-  }
-  if (B_ij_flag == FALSE) {
-    param_u = param_d = param = param_l <- NA
-  }
-  if (dir_distance_type == "symmetric") {
-    param_u = param_d <- NA
-  }
-  if (dir_distance_type == "asymmetric") {
-    param <- NA
-  }
-  if (disp_type == "leptokurtic") {
-    param_u = param_d = param <- NA
-  }
-  igraph::V(graph)$name <- igraph::vertex_attr(graph, nodes_id)
-  if (c_ij_flag == TRUE) {
-    c_ij_mat <- c_ij_fun(graph, dir_fragmentation_type = dir_fragmentation_type, 
-                         pass_confluence = pass_confluence, pass_u = pass_u, 
-                         pass_d = pass_d)
-  }
-  if (B_ij_flag == TRUE) {
-    B_ij_mat <- B_ij_fun(graph, field_B = field_B, dir_distance_type = dir_distance_type, 
-                         disp_type = disp_type, param_u = param_u, param_d = param_d, 
-                         param = param, param_l = param_l)
-  }
-  if (c_ij_flag == TRUE & B_ij_flag == TRUE) {
-    agg_mat <- c_ij_mat * B_ij_mat
-  }
-  if (c_ij_flag == TRUE & B_ij_flag == FALSE) {
-    agg_mat <- c_ij_mat
-  }
-  if (c_ij_flag == FALSE & B_ij_flag == TRUE) {
-    agg_mat <- B_ij_mat
-  }
-  g_v_df <- dplyr::rename_with(igraph::as_data_frame(graph, 
-                                                     what = "vertices"), ~"weight_node", contains(weight))
-  v_weights <- g_v_df$weight_node
-  if (index_type == "full") {
-    index_num = t(v_weights) %*% agg_mat %*% v_weights
-    index_den = sum(v_weights)^2
-    index = index_num/index_den
-    index <- data.frame(num = index_num, den = index_den, 
-                        index = index)
-  }
-  if (index_type == "reach") {
-    if (index_mode == "to") {
-      index_num = agg_mat %*% v_weights
-    }
-    if (index_mode == "from") {
-      index_num = t(t(v_weights) %*% agg_mat)
-    }
-    index_den = sum(v_weights)
-    index = index_num/index_den
-    index = data.frame(name = igraph::V(graph)$name, num = index_num, 
-                       den = index_den, index = index) %>% dplyr::rename_with(~nodes_id, 
-                                                                              contains("name"))
-  }
-  if (index_type == "sum") {
-    if (!("type" %in% igraph::edge_attr_names(graph))) 
-      stop("the graph's edges must contain the 'type' attribute with labels 'dam' or\n      'link' depending on the role of the edge (barrier or confluence).\n      Essential to calculate CAFI properly.")
-    igraph::E(graph)$pass_u <- igraph::get.edge.attribute(graph, 
-                                                          pass_u)
-    igraph::E(graph)$pass_d <- igraph::get.edge.attribute(graph, 
-                                                          pass_d)
-    igraph::E(graph)$pass_u <- ifelse(is.na(igraph::E(graph)$pass_u), 
-                                      pass_confluence, igraph::E(graph)$pass_u)
-    igraph::E(graph)$pass_d <- ifelse(is.na(igraph::E(graph)$pass_d), 
-                                      pass_confluence, igraph::E(graph)$pass_d)
-    if (dir_fragmentation_type == "symmetric") {
-      igraph::E(graph)$pass <- igraph::E(graph)$pass_d * 
-        igraph::E(graph)$pass_u
-    }
-    if (dir_fragmentation_type == "asymmetric" & index_mode == 
-        "to") {
-      igraph::E(graph)$pass <- igraph::E(graph)$pass_d
-    }
-    if (dir_fragmentation_type == "asymmetric" & index_mode == 
-        "from") {
-      igraph::E(graph)$pass <- igraph::E(graph)$pass_u
-    }
-    g_v_df <- dplyr::rename_with(igraph::as_data_frame(graph, 
-                                                       what = "vertices"), ~"weight_node", contains(weight))
-    g_e_df <- igraph::as_data_frame(graph, what = "edges") %>% 
-      dplyr::filter(.data$type == "dam") %>% dplyr::mutate(pass = 1 - 
-                                                             .data$pass) %>% dplyr::select(.data$from, .data$pass) %>% 
-      dplyr::rename(name = .data$from) %>% dplyr::left_join(g_v_df)
-    index = sum(g_e_df$pass * g_e_df$weight_node/max(g_v_df$weight_node))
-  }
-  return(index)
-}
-
-
-?c_ij_fun
+index[[1]]
